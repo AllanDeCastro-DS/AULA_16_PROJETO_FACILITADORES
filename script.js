14c83fe111c60eb02d0528fbf3b48aeb0d950cba
Allan de Castro <116167880+AllanDeCastro-DS@users.noreply.github.com>
2024-11-22 12:16:27 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function somar(a, b) {
+  let total = a + b;
+  console.log(total);
+}
+
+somar(2, 10);
+
+function subtrair(a, b) {
+  let total = a - b;
+  console.log(total);
+}
+
+subtrair(15, 17);
+
+let numeros = [1, 2, 3, 4, 10];
+
+numeros.map((item) => {
+  console.log(item);
+});
